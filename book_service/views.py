ce4c15d70082e6fd074674a208b252774618a08b
Sergii Semenets <serhey.semenets@gmail.com>
2024-08-13 21:24:39 +0300

    add filters for title & authors books

--- book_service/views.py
+++ book_service/views.py
@@ -10,3 +10,18 @@ class BookViewSet(viewsets.ModelViewSet):
     serializer_class = BookSerializer
     permission_classes = [IsAdminOrReadOnly]
 
+    def get_queryset(self):
+        """Retrieve the books with filters"""
+        title = self.request.query_params.get("title")
+        author = self.request.query_params.get("author")
+
+        queryset = self.queryset
+
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+
+        if author:
+            queryset = queryset.filter(author__icontains=author)
+
+        return queryset.distinct()
+
