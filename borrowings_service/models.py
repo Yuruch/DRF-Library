2bd6021ad593c074e1aeb515f6eb471bc3609ea3
Dima <dima2000yan@gmail.com>
2024-08-13 23:40:08 +0300

    borrowing model described, fixture added

--- borrowings_service/models.py
+++ borrowings_service/models.py
@@ -1,3 +1,14 @@
+from datetime import datetime
+
 from django.db import models
 
-# Create your models here.
+from book_service.models import Book
+from library_service import settings
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField(default=datetime.now)
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(null=True, blank=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
