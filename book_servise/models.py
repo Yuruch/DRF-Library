dc6996d3db9d2e68a80df6e25b91fb360df14dae
Sergii Semenets <serhey.semenets@gmail.com>
2024-08-13 11:37:19 +0300

    add book_crud

--- book_servise/models.py
+++ book_servise/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 
 class Cover(models.TextChoices):
-    hard = "HARD"
-    soft = "SOFT"
+    HARD = "Hard"
+    SOFT = "Soft"
 
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
-    cover = models.CharField(max_length=255, choices=Cover.choices)
+    cover = models.CharField(max_length=4, choices=Cover.choices)
     inventory = models.PositiveIntegerField()
     daily_fee = models.DecimalField(max_digits=5, decimal_places=2)
 
