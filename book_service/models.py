80f905e13f4cd472297f654411c0fd6970b6241f
Sergii Semenets <serhey.semenets@gmail.com>
2024-08-13 17:44:25 +0300

    add class choices cover

--- book_service/models.py
+++ book_service/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 
 class Book(models.Model):
-    COVER = [
-        ("HARD", "Hard cover"),
-        ("SOFT", "Soft cover")
-    ]
+    class Cover(models.TextChoices):
+        HARD = ("HARD", "Hard cover")
+        SOFT = ("SOFT", "Soft cover")
+
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
-    cover = models.CharField(max_length=4, choices=COVER)
+    cover = models.CharField(max_length=4, choices=Cover.choices)
     inventory = models.PositiveIntegerField()
     daily_fee = models.DecimalField(max_digits=5, decimal_places=2)
 
