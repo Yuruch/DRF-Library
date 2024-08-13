70d85a14c1b2884f36ef47a4d8db302bbe5533aa
Iryna Honcharenko <irynahoncharenko9@gmail.com>
2024-08-13 13:58:54 +0300

    Add authtoken

--- user/serializers.py
+++ user/serializers.py
@@ -5,13 +5,18 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ("id", "email", "password", "is_staff")
+        fields = ("id", "email", "first_name", "last_name", "password", "is_staff")
         read_only_fields = ("is_staff",)
         extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
 
     def create(self, validated_data):
         """Create a new user with encrypted password and return it"""
-        return get_user_model().objects.create_user(**validated_data)
+        return get_user_model().objects.create_user(
+            email=validated_data["email"],
+            first_name=validated_data["first_name"],
+            last_name=validated_data["last_name"],
+            password=validated_data["password"]
+        )
 
     def update(self, instance, validated_data):
         """Update a user, set the password correctly and return it"""
