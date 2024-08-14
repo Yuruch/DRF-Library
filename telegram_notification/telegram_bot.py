b2088a8eca373c2d5a5952899bbaccf8839f742b
necobra <necobrane105@gmail.com>
2024-08-14 14:56:44 +0300

    refactor TelegramBot class

--- telegram_notification/telegram_bot.py
+++ telegram_notification/telegram_bot.py
@@ -2,22 +2,19 @@ import os
 import requests
 
 from dotenv import load_dotenv
+import logging
 
 
+logger = logging.getLogger(__name__)
 load_dotenv()
 
 class TelegramBot:
-    def __init__(self):
+    def __init__(self) -> None:
+        self._chat_id = int(os.environ["ADMINISTRATION_CHAT_ID"])
         self._api_token = os.environ["TELEGRAM_BOT_KEY"]
-        self._chat_id = os.environ["ADMINISTRATION_CHAT_ID"]
-        if not self._chat_id or not self._chat_id.isnumeric():
-            # todo raise error that dont interupt whole django project
-            pass
-        if not self._api_token:
-            # todo raise error
-            pass
+        self.base_url = f"https://api.telegram.org/bot{self._api_token}"
 
-    def _send_message(self, to: int, message: str):
+    def _send_message(self, to: int, message: str) -> None:
         params = {
             "chat_id": to,
             "text": message
@@ -26,7 +23,9 @@ class TelegramBot:
             "Content-Type": "application/json"
         }
         request = requests.post(
-            f"https://api.telegram.org/bot{self._api_token}/sendMessage",
+            f"{self.base_url}/sendMessage",
             params=params,
             headers=headers
         )
+        if request.status_code != 200:
+            logging.warning(f"Message is not send: {request.content}")
