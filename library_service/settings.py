6e326e0c9a37341ea2e8c22c3c8fa86f442d4efe
Iryna Honcharenko <irynahoncharenko9@gmail.com>
2024-08-14 15:46:27 +0300

    Refactor AUTH_HEADER_NAME

--- library_service/settings.py
+++ library_service/settings.py
@@ -138,5 +138,5 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(days=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": True,
-    # "AUTH_HEADER_TYPES": ("Authorize",),
+    "AUTH_HEADER_NAME": "HTTP_AUTHORIZE",
 }
