33d4b59e60256d813f6d2b84ba44bada5d2f3e59
necobra <necobrane105@gmail.com>
2024-08-14 14:58:55 +0300

    add telegram notification for borrowing and payment

--- telegram_notification/telegram_bot.py
+++ telegram_notification/telegram_bot.py
@@ -8,6 +8,7 @@ import logging
 logger = logging.getLogger(__name__)
 load_dotenv()
 
+
 class TelegramBot:
     def __init__(self) -> None:
         self._chat_id = int(os.environ["ADMINISTRATION_CHAT_ID"])
@@ -15,17 +16,34 @@ class TelegramBot:
         self.base_url = f"https://api.telegram.org/bot{self._api_token}"
 
     def _send_message(self, to: int, message: str) -> None:
-        params = {
-            "chat_id": to,
-            "text": message
-        }
-        headers = {
-            "Content-Type": "application/json"
-        }
+        params = {"chat_id": to, "text": message}
+        headers = {"Content-Type": "application/json"}
         request = requests.post(
-            f"{self.base_url}/sendMessage",
-            params=params,
-            headers=headers
+            f"{self.base_url}/sendMessage", params=params, headers=headers
         )
         if request.status_code != 200:
             logging.warning(f"Message is not send: {request.content}")
+
+    def borrow_administration_notification(self, borrow: "Borrow"):
+        """send notification to administration about new and overdue borrow"""
+        message = (
+            f"Borrow date: {borrow.borrow_date}\n"
+            f"Expected return date: {borrow.expected_return_date}\n"
+            f"Book: {borrow.book}\n"
+            f"User: {borrow.user}"
+        )
+        self._send_message(self._chat_id, message)
+
+    def multiple_borrow_administration_notification(self, borrows: list["Borrow"]):
+        for borrow in borrows:
+            self.borrow_administration_notification(borrow)
+
+    def successful_payment_administration_notification(self, payment: "Payment"):
+        """send notification to administration about succesful payment"""
+        message = (
+            f"User: {payment.borrowing.user}\n"
+            f"Paid: {payment.type}\n"
+            f"For: {payment.borrowing.book}\n"
+            f"The amount: {payment.money_to_pay}$"
+        )
+        self._send_message(self._chat_id, message)
