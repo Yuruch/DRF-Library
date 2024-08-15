0d7052d11a4093af624353cbb95f220061bd66fb
necobra <necobrane105@gmail.com>
2024-08-15 13:48:41 +0300

    add localhost to allow hosts

--- library_service/settings.py
+++ library_service/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-!b1w3@xe2m)o!mb&ek(ji-zd@*yt5b4wumy$&!oq&gj*^!u2=9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 ngrok_url = get_ngrok_url()
 if ngrok_url:
