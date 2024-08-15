513d8cbd4835878b27730922b9ff84ea894165aa
necobra <necobrane105@gmail.com>
2024-08-15 12:54:33 +0300

    refactor encoding logic

--- telegram_notification/views.py
+++ telegram_notification/views.py
@@ -22,11 +22,11 @@ class RecieveConfirmationFromTelegram(views.APIView):
             return Response(status=status.HTTP_204_NO_CONTENT)
 
         telegram_id = request.data.get("message").get("from").get("id")
-        user_id, signature = (
-            request.data.get("message").get("text").split()[1].split("#")
-        )
-
-        if not telegram_id or not user_id or not user_id.isnumeric() or not signature:
+        try:
+            user_id, signature = (
+                request.data.get("message").get("text").split()[1].split("_")
+            )
+        except ValueError:
             return Response(
                 {"error": f"Invalid data"},
                 status=status.HTTP_400_BAD_REQUEST,
@@ -34,10 +34,10 @@ class RecieveConfirmationFromTelegram(views.APIView):
 
         if (
             signature
-            != hashlib.sha256(f"{user_id}{SECRET_PHRASE}".encode()).hexdigest()
+            != hashlib.sha256(f"{user_id}{SECRET_PHRASE}".encode()).hexdigest()[16:]
         ):
             return Response(
-                {"error": f"Invalid signature"}, status=status.HTTP_403_FORBIDDEN
+                {"error": f"Invalid signature"}, status=status.HTTP_400_BAD_REQUEST
             )
 
         user_id = int(user_id)
@@ -62,8 +62,10 @@ class ObtainTelegramConnectionURL(views.APIView):
     def get(self, request, *args, **kwargs):
         user_id = request.user.id
 
-        signature = hashlib.sha256(f"{user_id}{SECRET_PHRASE}".encode()).hexdigest()
-        connect_url = f"{os.environ['BOT_URL']}?start={user_id}#{signature}"
+        signature = hashlib.sha256(f"{user_id}{SECRET_PHRASE}".encode()).hexdigest()[
+            16:
+        ]
+        connect_url = f"{os.environ['BOT_URL']}?start={user_id}_{signature}"
         return Response(
             {"your url to connect telegram": connect_url}, status=status.HTTP_200_OK
         )
