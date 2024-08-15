4d48c3010c2b204ac0f926ff99431f13ea862bf1
necobra <necobrane105@gmail.com>
2024-08-15 11:54:19 +0300

    add obtain telegram connection url view & signature for confirming

--- telegram_notification/views.py
+++ telegram_notification/views.py
@@ -1,7 +1,16 @@
+import os
+import hashlib
+
 from django.contrib.auth import get_user_model
 from rest_framework import views, status
 from rest_framework.response import Response
 from telegram_notification.telegram_bot import TelegramBot
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+SECRET_PHRASE = os.environ["SECRET_PHRASE"]
 
 
 class ConnectTelegramView(views.APIView):
@@ -10,12 +19,22 @@ class ConnectTelegramView(views.APIView):
             return Response(status=status.HTTP_204_NO_CONTENT)
 
         telegram_id = request.data.get("message").get("from").get("id")
-        user_id = request.data.get("message").get("text").split()[1]
+        user_id, signature = (
+            request.data.get("message").get("text").split()[1].split("#")
+        )
 
-        if not telegram_id or not user_id or not user_id.isnumeric():
+        if not telegram_id or not user_id or not user_id.isnumeric() or not signature:
             return Response(
                 {"error": f"Invalid data"},
                 status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        if (
+            signature
+            != hashlib.sha256(f"{user_id}{SECRET_PHRASE}".encode()).hexdigest()
+        ):
+            return Response(
+                {"error": f"Invalid signature"}, status=status.HTTP_403_FORBIDDEN
             )
 
         user_id = int(user_id)
@@ -32,3 +51,14 @@ class ConnectTelegramView(views.APIView):
             return Response(
                 {"error": "User not found"}, status=status.HTTP_404_NOT_FOUND
             )
+
+
+class ObtainTelegramConnectionURL(views.APIView):
+    def get(self, request, *args, **kwargs):
+        user_id = request.user.id
+
+        signature = hashlib.sha256(f"{user_id}{SECRET_PHRASE}".encode()).hexdigest()
+        connect_url = f"{os.environ['BOT_URL']}?start={user_id}#{signature}"
+        return Response(
+            {"your url to connect telegram": connect_url}, status=status.HTTP_200_OK
+        )
